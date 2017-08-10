4dccec8e1183d7381d0c83e05da692707f65bbc5
Long Nguyen <litpuvn@gmail.com>
2017-08-10 16:25:46 -0500

    add stringAsFactor=false to avoid incorrect format

--- flu-shot-solution.R
+++ flu-shot-solution.R
@@ -1,6 +1,29 @@
 # flu-shot analysis
 
 # step1. loead data into memory
-tweets = read.csv("flu-shot.converted.csv", stringsAsFactors = FALSE)
+tweets = read.csv("flu-shot.converted.csv", stringsAsFactors = FALSE, header = FALSE)
 
+# add header
+colnames(tweets) = c('tweet', 'favorite_count')
+
+str(tweets)
+
+tweets
+
+library(tm)
+# Create Corpus
+corpus = Corpus(VectorSource(tweets$tweet))
+
+# remove 
+
+library(tidytext)
+
+library(tidyr)
+get_sentiments("bing")
+
+data("stop_words")
+
+unnest_tokens(tweets, "word", "tweet")
+
+anti_join(tweets, stop_words)
 
