5f48ba56212b1f21a6e2cb30e3953b6debf73968
Long Nguyen <litpuvn@gmail.com>
2017-08-11 16:47:44 -0500

    add function to calculate sentiment score

--- flu-shot-solution.R
+++ flu-shot-solution.R
@@ -11,14 +11,11 @@ str(tweets)
 tweets
 
 library(tm)
-# Create Corpus
-corpus = Corpus(VectorSource(tweets$tweet))
-
-# remove 
-
 library(tidytext)
-
 library(tidyr)
+library(dplyr)
+library(SnowballC)
+
 get_sentiments("bing")
 
 data("stop_words")
@@ -33,7 +30,50 @@ str(cleanTweets)
 
 count(cleanTweets, word, sort = TRUE) 
 
-bing_sentiments = get_sentiments("bing");
+bing_sentiments = get_sentiments("afinn");
 sentimentTweets = inner_join(cleanTweets, bing_sentiments, by="word")
 
 str(sentimentTweets)
+
+
+getTweetSentimentScore = function(message) {
+  text <- c(message)
+  text_df <- data_frame(line = 1:1, text = text)
+  tidyMessage = unnest_tokens(text_df, "word", "text")
+  
+  data("stop_words")
+  
+  # remove stop words
+  cleanMessage = anti_join(tidyMessage, stop_words, by="word")
+  
+  mySentiments = get_sentiments("afinn");
+  
+  mySentiments = mutate(mySentiments, word = wordStem(word))
+  mySentiments = mySentiments[!duplicated(mySentiments),]
+  #bing_sentiments = unique(bing_sentiments)
+
+  # stemming, sacasm, etc using SnowballC
+  cleanMessage = mutate(cleanMessage, word = wordStem(word))
+  
+  #create negative, positive words
+  sentimentMessage = inner_join(cleanMessage, mySentiments, by="word")
+
+  #sentiment score = counting negative, positive and make decision of the message sentiment
+  return (sum(sentimentMessage$score))
+}
+
+tweets$tweet[1]
+             
+st = getTweetSentimentScore("i am feeling happy and lucky")
+st
+
+
+?count
+count(st, word, sort = T)
+str(st);
+?mutate
+st
+
+View(st)
+
+
