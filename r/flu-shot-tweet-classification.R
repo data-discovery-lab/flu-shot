f9400747eb44ca2880386767b26dab865ba1025f
Long Nguyen <litpuvn@gmail.com>
2017-09-01 22:48:26 -0500

    adding classification

--- r/flu-shot-tweet-classification.R
+++ r/flu-shot-tweet-classification.R
@@ -25,7 +25,8 @@ library(ggplot2)
 library(stringr)
 
 set.seed(123)
-setwd("~/TTU-SOURCES/flu-shot")
+#setwd("~/TTU-SOURCES/flu-shot")
+setwd("~/Desktop/SOURCES/flue-shot")
 
 tweets = read.csv("labeled-tweet-flu-shot.csv", stringsAsFactors = FALSE)
 
@@ -39,78 +40,64 @@ replace_reg = "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|
 unnest_reg = "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
 
 tweets = tweets %>% 
-  dplyr::filter(!str_detect(tweet, "^RT")) %>%
-  mutate(text = str_replace_all(tweet, replace_reg, "")) %>%
+  mutate(text = str_replace_all(tweet, replace_reg, ""))
 
+tweets$tweet = tweets$text
+tweets$text = NULL
 
 str(tweets)
-
-
-# install and include library tm
-install.packages("tm")
-library(tm)
 
 
 # Create Corpus
 tweetCorpus = Corpus(VectorSource(tweets$tweet))
 
 # convert documents into lowercase
-tweetCorpus = tm_map(tweetCorpus, tolower)
+tweetCorpus = tm_map(tweetCorpus, content_transformer(tolower))
+tweetCorpus = tm_map(tweetCorpus, content_transformer(removePunctuation))
 
 str(tweetCorpus)
 
-# Remove Stop words & stemp document
-install.packages("SnowballC")
-library(SnowballC)
-tweetCorpus = tm_map(tweetCorpus, removeWords, stopwords("english"))
+tweetCorpus = tm_map(tweetCorpus, content_transformer(removeWords), c("flu", "shot", "shots", "feel", "like", "thank", "can", "may", "get", "got", "gotten", "think", "flushot", stopwords("english")))
 tweetCorpus = tm_map(tweetCorpus, stemDocument)
 
 dtmTweets = DocumentTermMatrix(tweetCorpus)
 dtmTweets
 
+# find frequency
 findFreqTerms(dtmTweets, lowfreq = 20)
+
 
 # Filter out sparse terms by keeping only terms that appear in 0.3% or more of the revisions
 sparseTweets = removeSparseTerms(dtmTweets, 0.997)
-
+sparseTweets
 
 cleanTweets = as.data.frame(as.matrix(sparseTweets))
-cleanTweets$takeFluShot = tweets$Take.flu.shot
-
 colnames(cleanTweets) = make.names(colnames(cleanTweets))
+cleanTweets$negativeFlushot = tweets$negativeFlushot
 
 str(cleanTweets)  
 
 library(caTools)
-set.seed(456)
 # create split with 70% is TRUE (this will be used as training set)
-spl = sample.split(cleanTweets$takeFluShot, SplitRatio = 0.7)
-train = subset(cleanTweets, spl == TRUE)
-test = subset(cleanTweets, spl == FALSE)
-# baseline model accuracy on the test set that always predicts "not vandalism"
-table(test$takeFluShot)[1] / sum(table(test$takeFluShot))
+spl = sample.split(cleanTweets$negativeFlushot, SplitRatio = 0.7)
+trainSparse = subset(cleanTweets, spl == TRUE)
+testSparse = subset(cleanTweets, spl == FALSE)
 
-
-
+str(testSparse)
 # CART Model
 library(rpart)
 library(rpart.plot)
-trialsCART <- rpart(trial~., data=train, method="class")
-prp(trialsCART)
+tweetCART <- rpart(negativeFlushot ~., data=trainSparse, method="class")
+prp(tweetCART)
 
 
 # Predict using the trainig set. Because the CART tree assigns the same predicted probability to each leaf node and there are a small number of leaf nodes compared to data points, we expect exactly the same maximum predicted probability.
-predTrain <- predict(trialsCART)[,2]
-# Accuracy on the training set
-t1 <- table(train$trial, predTrain >= 0.5)
-(t1[1,1] + t1[2,2])/(sum(t1))
+predictCart <- predict(tweetCART, newData=testSparse, type="class")
+
+str(predictCart)
+
+str(testSparse$negativeFlushot)
+table(testSparse$negativeFlushot, predictCart)
 
 
-
-### convert to short tweet
-mdat <- matrix(c(1,2,3, 11,12,13), nrow = 2, ncol=3, byrow=TRUE, 
-               dimnames = list(c("row1", "row2"), c("C.1", "C.2", "C.3")))
-
-## get columns of item
-colnames(mdat)[apply(mdat, 2, function(u) any(u < 3))]
 
