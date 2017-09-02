7723fdb0cc89488962bc95ba9ee891f929fe77ef
Long Nguyen <litpuvn@gmail.com>
2017-09-02 09:42:57 -0500

    create function to reuse

--- r/flu-shot-tweet-classification.R
+++ r/flu-shot-tweet-classification.R
@@ -31,53 +31,60 @@ setwd("~/Desktop/SOURCES/flue-shot")
 tweets = read.csv("labeled-tweet-flu-shot.csv", stringsAsFactors = FALSE)
 
 str(tweets)
+sum(tweets$negativeFlushot)
 
-version
+createDTMCleanTweet = function(tweets) {
+  
+  replace_reg = "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
+  unnest_reg = "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
+  
+  tweets = tweets %>% 
+    mutate(text = str_replace_all(tweet, replace_reg, ""))
+  
+  tweets$tweet = tweets$text
+  tweets$text = NULL
+  
+  str(tweets)
+  
+  
+  # Create Corpus
+  tweetCorpus = Corpus(VectorSource(tweets$tweet))
+  
+  # convert documents into lowercase
+  tweetCorpus = tm_map(tweetCorpus, content_transformer(tolower))
+  tweetCorpus = tm_map(tweetCorpus, content_transformer(removePunctuation))
+  
+  str(tweetCorpus)
+  
+  tweetCorpus = tm_map(tweetCorpus, content_transformer(removeWords), c("flu", "shot", "shots", "feel", "like", "thank", "can", "may", "get", "got", "gotten", "think", "flushot", stopwords("english")))
+  tweetCorpus = tm_map(tweetCorpus, stemDocument)
+  
+  dtmTweets = DocumentTermMatrix(tweetCorpus)
+  dtmTweets
+  
+  # find frequency
+  findFreqTerms(dtmTweets, lowfreq = 20)
+  
+  
+  # Filter out sparse terms by keeping only terms that appear in 0.3% or more of the revisions
+  sparseTweets = removeSparseTerms(dtmTweets, 0.997)
+  sparseTweets
+  
+  cleanTweets = as.data.frame(as.matrix(sparseTweets))
+  colnames(cleanTweets) = make.names(colnames(cleanTweets))
+  cleanTweets$negativeFlushot = tweets$negativeFlushot
+  
+  str(cleanTweets)  
+  
+  return(cleanTweets)
+}
 
 
-
-replace_reg = "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
-unnest_reg = "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
-
-tweets = tweets %>% 
-  mutate(text = str_replace_all(tweet, replace_reg, ""))
-
-tweets$tweet = tweets$text
-tweets$text = NULL
-
-str(tweets)
-
-
-# Create Corpus
-tweetCorpus = Corpus(VectorSource(tweets$tweet))
-
-# convert documents into lowercase
-tweetCorpus = tm_map(tweetCorpus, content_transformer(tolower))
-tweetCorpus = tm_map(tweetCorpus, content_transformer(removePunctuation))
-
-str(tweetCorpus)
-
-tweetCorpus = tm_map(tweetCorpus, content_transformer(removeWords), c("flu", "shot", "shots", "feel", "like", "thank", "can", "may", "get", "got", "gotten", "think", "flushot", stopwords("english")))
-tweetCorpus = tm_map(tweetCorpus, stemDocument)
-
-dtmTweets = DocumentTermMatrix(tweetCorpus)
-dtmTweets
-
-# find frequency
-findFreqTerms(dtmTweets, lowfreq = 20)
-
-
-# Filter out sparse terms by keeping only terms that appear in 0.3% or more of the revisions
-sparseTweets = removeSparseTerms(dtmTweets, 0.997)
-sparseTweets
-
-cleanTweets = as.data.frame(as.matrix(sparseTweets))
-colnames(cleanTweets) = make.names(colnames(cleanTweets))
-cleanTweets$negativeFlushot = tweets$negativeFlushot
-
-str(cleanTweets)  
+cleanTweets = createDTMCleanTweet(tweets)
 
 library(caTools)
+set.seed(456)
+
 # create split with 70% is TRUE (this will be used as training set)
 spl = sample.split(cleanTweets$negativeFlushot, SplitRatio = 0.7)
 trainSparse = subset(cleanTweets, spl == TRUE)
@@ -93,6 +100,7 @@ prp(tweetCART)
 
 # Predict using the trainig set. Because the CART tree assigns the same predicted probability to each leaf node and there are a small number of leaf nodes compared to data points, we expect exactly the same maximum predicted probability.
 predictCart <- predict(tweetCART, newdata=testSparse, type="class")
+summary(predictCart)
 
 str(predictCart)
 str(testSparse$negativeFlushot)
@@ -107,7 +115,19 @@ levels(predictCart)
 summary(predictCart)
 
 ## convert to ordered item, so that we can map to original data
-as.integer(as.character(predictCart))
+orderedCategories = as.integer(as.character(predictCart))
 
+## load complete data file and do prediction, then categorize each tweet
+completeTweets = read.csv("all-tweets-2014.csv", stringsAsFactors = FALSE)
 
+str(completeTweets)
 
+nrow(completeTweets)
+str(completeTweets)
+
+dtmCompleteTweets = createDTMCleanTweet(completeTweets)
+
+str(dtmCompleteTweets)
+predictCompleteTweets <- predict(tweetCART, newdata=cleanTweets, type="class")
+
+summary(predictCompleteTweets)
