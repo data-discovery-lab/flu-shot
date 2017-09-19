bfab54847a3d1abff7eeee74bed1bd58e01d1374
dsalopek <dylan.salopek@gmail.com>
2017-09-19 14:22:55 -0500

    tweet preprocessing
    - includes .py script for preprocessing data (contractions, spell
    checking, affirmation, wordnormalization)
    - includes dictionaries for functions within .py script
    - includes uncleaned data as well as cleaned data

--- src/neg-convert/convert.py
+++ src/neg-convert/convert.py
@@ -1,63 +1,59 @@
-# Andrew Dylan Salopek
-# 9/6/2017
-# Texas Tech Univeristy
+# Andrew Salopek
+# Texas Tech University
 
-# import csv module for r/w
 import csv
-
-
-# checks tweet for synonyms provided in a CSV table and
-# replaces synonyms with the more common word
-def synonym(inTweet: str) -> str:
-    splitTweet = inTweet.split()
-    convertedString = inTweet
-    with open('synonyms.csv') as syncsv:
-        dictionary = csv.DictReader(syncsv)
-        for entry in dictionary:
-            for word in splitTweet:
-                if entry['previous'] == word:
-                    wordIndex = splitTweet.index(entry['previous'])
-                    splitTweet[wordIndex] = entry['synonym']
-                    convertedString = " ".join(splitTweet)
-        return convertedString
-
-
-# checks tweet for negative phrase, eg "not fun"
-# and replaces it with affirmative synonym, eg "unpleasant"
-def affirm(inTweet: str) -> str:
-    with open('opposites.csv') as dictcsv:
-        dictionary = csv.DictReader(dictcsv)
-        i = 0
-        for entry in dictionary:
-            wordlist = inTweet.split()
-            if "not" in wordlist:
-                try:
-                    next = wordlist[wordlist.index("not") + 1]
-                    if next == entry['previous']:
-                        phrase = "not " + next
-                        convertedString = inTweet.replace(phrase, entry['opposite'])
-                        return synonym(convertedString)
-                except IndexError:
-                    print("Error")
-                    return synonym(inTweet)
-        return synonym(inTweet)
+import re
+import enchant
 
 
 def contraction(inTweet: str) -> str:
-    splitTweet = inTweet.split()
-    convertedString = inTweet
-    with open('contractions.csv') as contcsv:
-        dictionary = csv.DictReader(contcsv)
-        for entry in dictionary:
-            for word in splitTweet:
-                if entry['contraction'] == word:
-                    wordIndex = splitTweet.index(entry['contraction'])
-                    splitTweet[wordIndex] = entry['separate']
-                    convertedString = " ".join(splitTweet)
-        return affirm(convertedString)
+    with open('contractions.csv') as f:
+        f.readline()  # ignore first line (header)
+        mydict = dict(csv.reader(f, delimiter=','))
+        pattern = re.compile(r'\b(' + '|'.join(mydict.keys()) + r')\b')
+        result = re.sub(pattern, lambda m: mydict.get(m.group(), m.group()), inTweet)
+    return spellCheck(result)
 
 
-# creates new tweet data set with altered text
+def affirm(inTweet: str) -> str:
+    with open('opposites.csv') as f:
+        f.readline()  # ignore first line (header)
+        mydict = dict(csv.reader(f, delimiter=','))
+        pattern = re.compile(r'not\s(' + '|'.join(mydict.keys()) + r'\b)')
+        result = re.sub(pattern, lambda m: mydict.get(m.group(1)), inTweet)
+    return synonym(result)
+
+
+def synonym(inTweet: str) -> str:
+    with open('synonyms.csv') as f:
+        f.readline()  # ignore first line (header)
+        mydict = dict(csv.reader(f, delimiter=','))
+        pattern = re.compile(r'\b(' + '|'.join(mydict.keys()) + r')\b')
+        result = re.sub(pattern, lambda m: mydict.get(m.group(), m.group()), inTweet)
+    return result
+
+
+def spellCheck(inTweet):
+    dict = enchant.Dict("en-US")
+    outStr = []
+    wordList = inTweet.split()
+    for word in wordList:
+        word = re.sub('\!*\?*\.*\,*#*', '', word)
+        try:
+            if not (dict.check(word)):
+                try:
+                    newWord = dict.suggest(word)
+                    outStr.append(newWord[0])
+                except:
+                    outStr.append(word)
+            else:
+                outStr.append(word)
+        except ValueError:
+            pass
+    string = " ".join(outStr)
+    return affirm(string)
+
+
 with open('unalteredTweets.csv') as tweetcsv:
     tweets = csv.DictReader(tweetcsv)
     with open('convertedTweets.csv', 'w') as convertcsv:
@@ -65,9 +61,9 @@ with open('unalteredTweets.csv') as tweetcsv:
         csvwrite = csv.DictWriter(convertcsv, fieldnames=headers)
         csvwrite.writeheader()
         i = 0
+        spellCheck("")
         for row in tweets:
             tweet = contraction(row['tweet'].lower())
             print(i, tweet)
-            i += 1
             csvwrite.writerow({"user": row["user"], "tweet": tweet, "state": row["state"],
                                "takeFlushot": row["takeFlushot"], "negativeFlushot": row["negativeFlushot"]})
