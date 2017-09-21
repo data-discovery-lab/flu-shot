94dd7b8dbb501f5d3a6a29f147415dc982b7a674
Hoang Long <litpuvn@gmail.com>
2017-09-21 15:52:01 -0500

    visualize world cloud for non-processing model

--- r/word-cloud.R
+++ r/word-cloud.R
@@ -1,27 +1,49 @@
 library(wordcloud2)
 library(SnowballC)
+library(stringr)
 
-words = read.csv(file = "tweet-word-transaction.csv", sep = ",")
-words$item[words$item == 'dead'] = 'die'
-words$item[words$item == 'paralyzed'] = 'paralyze'
+# without pre-processing data
+setwd("~/TTU-SOURCES/flu-shot")
+
+tweets = read.csv("labeled-tweet-flu-shot.csv", stringsAsFactors = FALSE)
+
+cleanTweet = function(tweets) {
+  replace_reg = "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
+  unnest_reg = "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
+  
+  tweets = tweets %>% 
+    mutate(text = str_replace_all(tweet, replace_reg, ""))
+  
+  tweets$tweet = tweets$text
+  tweets$text = NULL
+  
+  return(tweets)
+}
+
+tweets = cleanTweet(tweets)
+
+str(tweets)
+
+additionalStopWords = c("flu", "shot", "shots", "person", "family", "hospital", "hospital")
+additionalStopWords_df <- data_frame(lexicon="custom", word = additionalStopWords)
+
+
+custom_stop_words = stop_words
+custom_stop_words <- bind_rows(custom_stop_words, additionalStopWords_df)
+
+
+words = tweets %>%
+  unnest_tokens(word, tweet) %>%
+  anti_join(custom_stop_words, by = c("word" = "word")) %>%
+  mutate(word = wordStem(word))
 
 
 str(words)
 
 ## visualize negative flu-shot words
-wordFreq = count(words[words$negativeFlushot == 1,], item, sort = TRUE)
-
-colnames(wordFreq) = c("word", "freq")
-
-str(wordFreq)
-
-wordcloud2(data = wordFreq)
-
-
-
-## visualize non-negative flu shot word
-wordFreq = count(words[words$negativeFlushot == 0,], item, sort = TRUE)
-
+wordFreq = count(words[words$negativeFlushot == 1,], word, sort = TRUE) %>%
+  filter(n >= 3) 
+  
 colnames(wordFreq) = c("word", "freq")
 
 str(wordFreq)
