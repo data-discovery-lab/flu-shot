79664d1769cec19636475b1ad37db7cca0cbeb49
Hoang Long <litpuvn@gmail.com>
2017-09-21 15:01:04 -0500

    fix mini word cloud

--- r/mini-word-cloud.R
+++ r/mini-word-cloud.R
@@ -5,51 +5,35 @@ library(tidytext)
 
 setwd("~/TTU-SOURCES/flu-shot")
 
-text <- c("Flu shots do not work",
-          "Get the flu shot they said. You won't get sick",
-          "I work at a hospital and we have to get the flu shot. I'm only person in family to not get sick this year",
-          "I haven't had any type of flu shot or vaccine in years. I don't trust em",
-          "Another precious child dies from flu complications after getting flu shot!",
-          "Flu-related death toll doubles in Wash State"
-          )
-
-
+# text <- c("Flu shots do not work",
+#           "Get the flu shot they said. You won't get sick",
+#           "I work at a hospital and we have to get the flu shot. I'm only person in family to not get sick this year",
+#           "I haven't had any type of flu shot or vaccine in years. I don't trust em",
+#           "Another precious child dies from flu complications after getting flu shot!",
+#           "Flu-related death toll doubles in Wash State",
+#           "nobody dies because of flu shot this year",
+#           "flu shot is not hurt at all"
+#           )
 
 text <- c("flu shot do ineffective",
 "get the flu shot they said you will avoid sick",
 "i work at a hospital and we have to get the flu shot I'm only person in family to avoid sick this year",
-"have not had any type of flu shot or flu shot in years i do suspect em",
+"i have not had any type of flu shot or flu shot in years i do suspect em",
 "Another precious child die from flu complications after getting flu shot!",
-"Flu-related die toll doubles in Wash State"
+"flu-related die toll doubles in Wash State",
+"everyone survive because of flu shot this year",
+  "flu shot is painless at all"
 )
 
+additionalStopWords = c("flu", "shot", "shots", "person", "family", "hospital", "hospital")
+additionalStopWords_df <- data_frame(lexicon="custom", text = additionalStopWords)
 
 
 custom_stop_words = stop_words
-custom_stop_words <- bind_rows(custom_stop_words,
-                               data_frame(word = "flu",
-                                          lexicon = "custom"))
-custom_stop_words <- bind_rows(custom_stop_words,
-                               data_frame(word = "shot",
-                                          lexicon = "custom"))
-custom_stop_words <- bind_rows(custom_stop_words,
-                               data_frame(word = "shots",
-                                          lexicon = "custom"))
+custom_stop_words <- bind_rows(custom_stop_words, additionalStopWords_df)
 
-custom_stop_words <- bind_rows(custom_stop_words,
-                               data_frame(word = "person",
-                                          lexicon = "custom"))
-custom_stop_words <- bind_rows(custom_stop_words,
-                               data_frame(word = "family",
-                                          lexicon = "custom"))
 
-custom_stop_words <- bind_rows(custom_stop_words,
-                               data_frame(word = "vaccine",
-                                          lexicon = "custom"))
-
-custom_stop_words <- bind_rows(custom_stop_words,
-                               data_frame(word = "hospital",
-                                          lexicon = "custom"))
+text_df <- data_frame(line = 1:length(text), text = text)
 
 words =text_df %>%
   unnest_tokens(word, text) %>%
