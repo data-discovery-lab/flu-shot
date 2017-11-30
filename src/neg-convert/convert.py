7a85edba14ab630765c13b0415ca213fab2604c3
Andrew Salopek <dylan.salopek@gmail.com>
2017-11-30 13:53:44 -0600

    Made script universal
    - takes CSV file as command argument and makes a word frequency dictionary for word normalization and then converts text in dataset
    - the text that is to be converted must have a heading as 'text' in order to be converted
    - does not handle files that do not exist

--- src/neg-convert/convert.py
+++ src/neg-convert/convert.py
@@ -5,84 +5,81 @@ import csv
 import re
 import enchant
 import operator
+import sys
+import freq
+
 from stanfordcorenlp import StanfordCoreNLP
 
 nlp = StanfordCoreNLP('http://corenlp.run', port=80)
 
 
-def contraction(inTweet, sorted_d):
+def contraction(inData):
     with open('contractions.csv') as f:
         f.readline()  # ignore first line (header)
         mydict = dict(csv.reader(f, delimiter=','))
         pattern = re.compile(r'\b(' + '|'.join(mydict.keys()) + r')\b')
-        result = re.sub(pattern, lambda m: mydict.get(m.group(), m.group()), inTweet)
-    return parser(result, sorted_d)
+        result = re.sub(pattern, lambda m: mydict.get(m.group(), m.group()), inData)
+    # print(result, "contraction")
+    return parser(result)
 
 
-def affirm(inTweet, sorted_d):
+def affirm(inData):
     with open('opposites.csv') as f:
         f.readline()  # ignore first line (header)
         mydict = dict(csv.reader(f, delimiter=','))
         pattern = re.compile(r'not\s(' + '|'.join(mydict.keys()) + r'\s)')
-        result = re.sub(pattern, lambda m: mydict.get(m.group(1)), inTweet)
-    return synonym(result, sorted_d)
+        result = re.sub(pattern, lambda m: mydict.get(m.group(1)), inData)
+    # print(result, "affirm")
+    return synonym(result)
 
 
-def synonym(inTweet, sorted_d):
-    wordList = inTweet.split()
-    synArray = []
+def synonym(inData):
+    wordList = inData.split()
+    # print(wordList, "33")
+    with open(sys.argv[1] + '_freq.csv') as fd:
+        freqDict = dict(csv.reader(fd, delimiter=','))
     with open('synonyms.csv') as f:
         f.readline()  # ignore first line (header)
-        mydict = dict(csv.reader(f, delimiter=','))
+        syndict = dict(csv.reader(f, delimiter=','))
         for word in wordList:
-            if word in mydict.values():
+            synArray = []
+            if word in syndict.values():
                 synArray.append(word)
-                while (word):
-                    try:
-                        word = mydict[word]
-                        # synArray.append(word)
-                        if word in mydict.keys():
-                            synArray.append(word)
-                        else:
-                            synArray.append(word)
-                    except:
-                        break
-            if (synArray):
-                maxCnt = 0
-                maxWord = synArray[0]
-                for element in synArray:
-                    if dict(sorted_d)[element]:
-                        if (dict(sorted_d)[element] > maxCnt):
-                            maxCnt = dict(sorted_d)[element]
-                            maxWord = element
-                else:
+                try:
+                    word = syndict[word]
+                except:
                     pass
-                wordList = [w.replace(word,maxWord) for w in wordList]
-                word = maxWord
-        print("62",wordList)
+        if (synArray):
+            maxCnt = 0
+            maxWord = synArray[0]
+            for element in synArray:
+                try:
+                    if freqDict[element]:
+                        if (freqDict[element] > maxCnt):
+                            maxCnt = freqDict[element]
+                            maxWord = element
+                        else:
+                            pass
+                    else:
+                        pass
+                except:
+                    pass
+            wordList = [w.replace(word, maxWord) for w in wordList]
+    return ' '.join(wordList)
 
-                    #
-                    #
-                    #
-                    # else:
-                    #     print("false")
-                    # pattern = re.compile(r'\b(' + '|'.join(mydict.keys()) + r')\b')
-                    # result = re.sub(pattern, lambda m: mydict.get(m.group(), m.group()), inTweet)
-                    # return result
 
-
-def spellCheck(inTweet,sorted_d):
-    newTweet = re.sub(r'!*\?*', '', inTweet)
-    dict = enchant.Dict("en-US")
+def spellCheck(inData):
+    newData = re.sub(r'!*\?*', '', inData)
+    spellDict = enchant.Dict("en-US")
     outStr = []
     pattern = re.compile(r'^[(@|#]\w+')
-    wordList = newTweet.split()
+    wordList = newData.split()
     for word in wordList:
         if not (pattern.match(word)):
             try:
-                if not (dict.check(word)):
+                if not (spellDict.check(word)):
                     try:
-                        newWord = dict.suggest(word)
+                        newWord = spellDict.suggest(word)
                         outStr.append(newWord[0])
                     except:
                         outStr.append(word)
@@ -93,48 +90,30 @@ def spellCheck(inTweet,sorted_d):
         else:
             outStr.append(word)
     string = " ".join(outStr)
-    return contraction(string, sorted_d)
+    return contraction(string)
 
 
-def parser(inTweet, sorted_d):
-    # wordlist = inTweet.split()
-    dep = nlp.dependency_parse(inTweet)
+def parser(inData):
+    dep = nlp.dependency_parse(inData)
     result = [item for item in dep if item[0] == "neg"]
     if result:
-        for item in result:
-            # first = item[2] - 1
-            # second = item[1] - 1
-            # print(wordlist[first], wordlist[second])
-            contr = affirm(inTweet, sorted_d)
-            return synonym(contr, sorted_d)
+        contr = affirm(inData)
+        print(result, "parser-result")
+        return contr
     else:
-        return synonym(inTweet, sorted_d)
+        print(result, "parser-noresult")
+        return synonym(inData)
 
 
-def sort(tweets):
-    if (tweets):
-        for row in tweets:
-            match_pattern = re.findall(r'\b[a-z]{3,15}\b', row['tweet'].lower())
-            for word in match_pattern:
-                count = frequency.get(word, 0)
-                frequency[word] = count + 1
-            frequency_list = frequency.keys()
-            sorted_d = sorted(frequency.items(), key=operator.itemgetter(1), reverse=True)
-        return sorted_d
-
-
-with open('unalteredTweets.csv') as tweetcsv:
-    tweets = csv.DictReader(tweetcsv)
-    frequency = {}
-    sorted_d = sort(tweets)
-    with open('convertedTweets.csv', 'w') as convertcsv:
-        headers = ["user", "tweet", "date", "state", "takeFlushot", "negativeFlushot"]
+with open(sys.argv[1]) as textcsv:
+    text = csv.DictReader(textcsv)
+    with open(sys.argv[1] + '_converted.csv', 'w') as convertcsv:
+        headers = ["oldtext", "newtext"]
         csvwrite = csv.DictWriter(convertcsv, fieldnames=headers)
         csvwrite.writeheader()
         i = 0
-        for row in tweets:
-            tweet = spellCheck(row['tweet'].lower(), sorted_d)
-            print(i, tweet)
+        for row in text:
+            line = spellCheck(row['text'].lower())
+            print(i, line)
             i += 1
-            csvwrite.writerow({"user": row["user"], "tweet": tweet[0], "date": row["date"], "state": row["state"],
-                               "takeFlushot": row["takeFlushot"], "negativeFlushot": row["negativeFlushot"]})
+            csvwrite.writerow({"oldtext": row['text'], "newtext": line})
---
+++ src/neg-convert/freq.py
@@ -0,0 +1,23 @@
+import sys
+import csv
+import re
+
+
+def freqSort(text):
+    frequency = {}
+    if (text):
+        for row in text:
+            match_pattern = re.findall(r'\b[a-z]{3,15}\b', row['text'].lower())
+            for word in match_pattern:
+                count = frequency.get(word, 0)
+                frequency[word] = count + 1
+    with open(sys.argv[1] + '_freq.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in frequency.items():
+            writer.writerow([key, value])
+
+
+print(sys.argv[1])
+with open(sys.argv[1]) as textcsv:
+    text = csv.DictReader(textcsv)
+    freqSort(text)
